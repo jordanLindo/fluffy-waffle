0bde794f15a502310c3be0d91c53274290ce1de6
jordanLindo <jordanlindo@live.com>
2022-06-22 14:25:05 -0500

    Extend project and Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fluffy-waffle
+
 A project to practice using an adjusted gitignore and linked files
+Add in a Top 10 Ghibli Movies list, altered the experience section and added questions to the game.
--- js/app.js
+++ js/app.js
@@ -5,17 +5,21 @@
 */
 
 "use strict";
-
+// Global variables
 var globalName;
 
 /**
  * Initialize all the code when the page loads.
  */
-function initalize(){
-    //console.log('in initializ()');
+function initialize(){
+    //console.log('in initialize()');
     globalName = getName();
 }
 
+/**
+ * Prompts the user for a name
+ * @returns string name
+ */
 function getName(){
     let name = prompt("Please enter your name.");
     alert('Welcome '+ name + '!');
@@ -24,52 +28,126 @@ function getName(){
     return name;
 }
 
+/**
+ * asks a set of questions
+ */
 function askQuestions(){
-    let firstAnswer = prompt("Do you think my favorite color is blue?");
-    if(firstAnswer.toLowerCase() === "no" || firstAnswer.toLowerCase() === "n"){
-        let element = document.getElementById("theBody");
-        element.classList.add("first-answer-correct");
-        alert("You are correct.");
-        //console.log("You are correct. Blue is not my favorite color.");
-    } else {
-        alert("You are incorrect.");
-        //console.log("You are incorrect. Blue is not my favorite color.")
+    let score = 0;
+    let promptMessage = "Yes or No";
+    let possibleAnswers = ["popcorn","reese's"];
+    let questions = [
+        [
+            "Do you think my favorite color is blue?",
+            "no",
+            "Blue is not my favorite color."
+        ],
+        [
+            "Do you think I like black coffee?",
+            "yes",
+            "I do like coffee black."
+        ],
+        [
+            "Do you think I like nature walks?",
+            "no",
+            "I do not care for nature walks."
+        ],
+        [
+            "Do you think I enjoy baking bread?",
+            "yes",
+            "I do enjoy baking bread."
+        ],
+        [
+            "Do you think I have any pets?",
+            "no",
+            "I do not have any pets."
+        ]
+    ];
+    for(let i = 0; i < questions.length; i++){
+        let answer = prompt(questions[i][0]+" "+promptMessage).toLowerCase();
+        let output = "You are incorrect. ";
+        if(checkAnswer(questions[i][1], answer)){
+            output = "You are correct. ";
+            score++;
+            if(i === 0){
+                let element = document.getElementById("theBody");
+                element.classList.add("first-answer-correct");
+            }
+        }
+        alert(output+questions[i][2]);
+        //console.log(output+questions[i][2]);
     }
-    let secondAnswer = prompt("Do you think I like black coffee?");
-    if(secondAnswer.toLowerCase() === "yes" 
-    || secondAnswer.toLowerCase() === "y"){
-        alert("You are correct.");
-        //console.log("You are correct. I do like coffee black.");
-    } else {
-        alert("You are incorrect.");
-        //console.log("You are incorrect. I do like coffee black.");
-    }
-    let thirdAnswer = prompt("Do you think I like nature walks?");
-    if(thirdAnswer.toLowerCase() === "no" 
-    || thirdAnswer.toLowerCase() === "n"){
-        alert("You are correct.");
-        //console.log("You are correct. I do not care for nature walks.");
-    } else {
-        alert("You are incorrect.");
-        //console.log("You are incorrect. I do not care for nature walks.")
-    }
-    let fourthAnswer = prompt("Do you think I enjoy baking bread?");
-    if(fourthAnswer.toLowerCase() === "yes" || fourthAnswer.toLowerCase() === "y"){
-        alert("You are correct.");
-        //console.log("You are correct. I do enjoy baking bread.");
-    } else {
-        alert("You are incorrect.");
-        //console.log("You are incorrect. I do enjoy baking bread.");
-    }
-    let fifthAnswer = prompt("Do you think I have any pets?");
-    if(fifthAnswer.toLowerCase() === "no" || fifthAnswer.toLowerCase() === "n"){
-        alert("You are correct.");
-        //console.log("You are correct. I do not have any pets.");
-    } else {
-        alert("You are incorrect.");
-        //console.log("You are incorrect. I do not have any pets.");
+    
+    //console.log("Guess a number");
+    let selectedNum = Math.floor((Math.random()*10)+1);
+    let guesses = 4;
+    while(guesses > 0){
+        let userGuess = prompt("Guess my number between 1 and 10. "
+        + guesses+ " tries left.")
+
+        if(userGuess == selectedNum){
+            alert("That is correct!");
+            score++;
+            break;
+        } else if(userGuess > selectedNum){
+            alert("too high");
+        } else if(userGuess < selectedNum){
+            alert("too low");
+        }
+
+        guesses--;
+        if(guesses === 0){
+            alert("The correct answer is "+selectedNum);
+        }
     }
 
+    //console.log("Guess a snack");
+    guesses = 6;
+    while(guesses > 0){
+        let snackGuess = prompt("Guess my theater snacks.").toLowerCase();
+        let done = false;
+        for(let i = 0; i < possibleAnswers.length; i++){
+            if(snackGuess == possibleAnswers[i]){
+                alert("That is one of my snacks!");
+                score++;
+                done = true;
+                break;
+            }
+        }
+        if(done){
+            break;
+        } else {
+            alert("That isn't one of my snacks.")
+        }
+        guesses--;
+        if(guesses === 0){
+            let resultString = "";
+            for(let i = 0; i < possibleAnswers.length; i++){
+                if(i > 0){
+                    resultString += ", ";
+                }
+                resultString += possibleAnswers[i];
+            }
+            alert(resultString);
+        }
+    }
+
+
+    alert("Your final score was " + score + ".");
     alert('That was fun ' + globalName + ". Let's play again sometime.");
+
+}
+
+function checkAnswer(correctAnswer, userResponse){
+    let result = false;
+    if(correctAnswer === "yes"){
+        if(userResponse === "yes" || userResponse === "y"){
+            result = true;
+        }
+    } else {
+        if(userResponse === "no" || userResponse === "n"){
+            result = true;
+        }
+    }
+    return result
 
 }
